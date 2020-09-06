560968d47286cbba053a782a1f37918e6ee3e72d
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-09-06 10:07:33 +0700

    end webpack react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Card from './Card';
+
 import './App.css';
 
 const App = () => (
   <div className='app'>
+    <Card/>
     <div className='card'>
       <h1 className='title'>Our React is working :)</h1>
     </div>
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <h1>Ini isinya seharusnya card sih. Cuma testing doang</h1>
+        </div>
+    )
+};
+
+export default Card;
